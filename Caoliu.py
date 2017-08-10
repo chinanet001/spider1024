566aca3c8a00243e548d95eeabdc3b81f06c2e6f
chuxiuhong <nanwugongzi@vip.qq.com>
2017-08-11 05:56:37 +0800

    init project

---
+++ Caoliu.py
@@ -0,0 +1,100 @@
+import requests
+import re
+import os
+import threading
+
+class Caoliu:
+    def __init__(self):
+        self.header_data = {
+            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+            'Accept-Encoding': '',
+            'Accept-Language': 'zh-CN,zh;q=0.8',
+            'Cache-Control': 'max-age=0',
+            'Connection': 'keep-alive',
+            'Host': 'www.t66y.com',
+            'Upgrade-Insecure-Requests': '1',
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36',
+        }
+        if "torrent_dir" not in os.listdir(os.getcwd()):
+            os.makedirs("torrent_dir")
+
+    def download_page(self, url):
+        header_data2 = {
+            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+            'Accept-Encoding': 'gzip, deflate',
+            'Accept-Language': 'zh-CN,zh;q=0.8',
+            'Cache-Control': 'max-age=0',
+            'Connection': 'keep-alive',
+            'Host': 'rmdown.com',
+            'Referer': 'http://www.viidii.info/?http://rmdown______com/link______php?' + url.split("?")[1],
+            'Upgrade-Insecure-Requests': '1',
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36'
+        }
+        try:
+            download_text = requests.get(url, headers=header_data2).text
+            p_ref = re.compile("name=\"ref\" value=\"(.+?)\"")
+            p_reff = re.compile("NAME=\"reff\" value=\"(.+?)\"")
+            ref = p_ref.findall(download_text)[0]
+            reff = p_reff.findall(download_text)[0]
+            r = requests.get("http://www.rmdown.com/download.php?ref="+ref+"&reff="+reff+"&submit=download")
+            with open("torrent_dir\\" + ref + ".torrent", "wb") as f:
+                f.write(r.content)
+        except:
+            print("download page " + url + " failed")
+
+    def index_page(self, fid=2, offset=1):
+        p = re.compile("<h3><a href=\"(.+?)\"")
+        try:
+            tmp_url = "http://www.t66y.com/thread0806.php?fid=" + str(fid) + "&search=&page=" + str(offset)
+            r = requests.get(tmp_url)
+            for i in p.findall(r.text):
+                self.detail_page(i)
+
+        except:
+            print("index page " + str(offset) + " get failed")
+
+    def detail_page(self, url):
+        p1 = re.compile("(http://rmdown.com/link.php.+?)<")
+        p2 = re.compile("(http://www.rmdown.com/link.php.+?)<")
+        base_url = "http://www.t66y.com/"
+        try:
+            r = requests.get(url=base_url + url, headers=self.header_data)
+            url_set = set()
+            for i in p1.findall(r.text):
+                url_set.add(i)
+            for i in p2.findall(r.text):
+                url_set.add(i)
+            url_list = list(url_set)
+            for i in url_list:
+                self.download_page(i)
+        except:
+            print("detail page " + url + " get failed")
+
+    def start(self, type, page_start=1, page_end=10, max_thread_num=10):
+        if type == "yazhouwuma":
+            fid = 2
+        elif type == "yazhouyouma":
+            fid = 15
+        elif type == "oumeiyuanchuang":
+            fid = 4
+        elif type == "dongmanyuanchuang":
+            fid = 5
+        elif type == "guochanyuanchuang":
+            fid = 25
+        elif type == "zhongziyuanchuang":
+            fid = 26
+        else:
+            raise ValueError("type wrong!")
+        max_thread_num = min(page_end-page_start+1,max_thread_num)
+        thread_list = []
+        for i in range(page_start, page_end + 1):
+            thread_list.append(threading.Thread(target=self.index_page, args=(fid, i,)))
+        for t in thread_list:
+            t.start()
+            while True:
+                if (len(threading.enumerate()) < max_thread_num):
+                    break
+if __name__ == "__main__":
+    c = Caoliu()
+    c.index_page()
+    c.start(type="dongmanyuanchuang",page_start=1,page_end=20,max_thread_num=50)
---
+++ README.md
@@ -0,0 +1,28 @@
+﻿# 草榴种子下载器
+
+项目依赖Python3,需要requests库。国内运行需要翻墙。此项目仅用于代码学习
+
+打开Caoliu.py，最后几行如下:
+```python
+if __name__ == "__main__":
+c = Caoliu()
+c.index_page()
+c.start(type="yazhouwuma",page_start=1,page_end=20,max_thread_num=50)
+```
+type参数负责下载类型，其对应如下:
+|下载类型|type|
+|:--:|:--:|
+|亚洲无码|yazhouwuma|
+|亚洲有码|yazhouyouma|
+|欧美原创|oumeiyuanchuang|
+|动漫原创|dongmanyuanchuang|
+|国产原创|guochanyuanchuang|
+|中字原创|zhongziyuanchuang|
+
+page_start,page_end代表起始页和终止页
+max_thread_num代表允许程序使用的最大线程数
+设定好各参数后，运行Caoliu.py。会在目录下创建一个torrent_dir，并开始下载种子。
+![image1](https://raw.githubusercontent.com/chuxiuhong/cloudphoto/master/1024.png)
+
+
+
